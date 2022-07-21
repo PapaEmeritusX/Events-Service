d373efe2d2f62c80b4fc138293cb376e85111fb6
karim <karim.p.87@gmail.com>
2022-07-21 15:59:44 +0500

    Revert "move MainClass to the root module"
    This reverts commit 6cf313d42cab4959b894c135a40d1f39754dde2c.

--- event-service-rest/src/main/java/events/DevelopmentConfig.java
+++ event-service-rest/src/main/java/events/DevelopmentConfig.java
@@ -3,7 +3,6 @@ package events;
 import events.data.Event;
 import events.data.Speaker;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import events.repository.EventRepository;
